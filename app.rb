9243cf0b3eb70604410b8b60aad8e6b6e2f1ecf1
NazirShaikh <shain003.212@gmail.com>
2021-08-16 18:09:10 +0100

    added signup page and test for it

--- app.rb
+++ app.rb
@@ -1,7 +1,10 @@
 require 'sinatra/base'
 
 class Makers_BnB < Sinatra::Base 
-
+  
+  get '/signup' do
+    erb(:signup)
+  end
 
   run! if app_file == $0
 end
---
+++ spec/features/signup_page_spec.rb
@@ -0,0 +1,28 @@
+require 'pg'
+
+feature 'signup page' do
+  scenario 'user can visit the signup page' do
+    visit('/signup')
+    expect(page).to have_content('Sign up to Makers BnB!')
+  end
+
+  scenario 'user can create an account which gets stored in the database' do
+    visit('/signup')
+    fill_in('username', with: 'Dillon')
+    fill_in('first_name', with: 'Dylan')
+    fill_in('second_name', with: 'Hancock')
+    fill_in('password', with: 'jellySNAKES123')
+    fill_in('email', with: 'Dillon@yahoo.co.uk')
+    click_button('Submit')
+ 
+    connection = PG.connect(dbname: 'makersbnb_test')
+    result = connection.exec("SELECT * FROM users;").add_a
+
+    expect(result[0]['username']).to eq('Dillon')
+    expect(result[0]['first_name']).to eq('Dylan')
+    expect(result[0]['second_name']).to eq('Hancock')
+    expect(result[0]['password']).to eq('jellySNAKES123')
+    expect(result[0]['email']).to eq('Dillon@yahoo.co.uk')
+
+  end
+end
