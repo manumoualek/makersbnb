a64cd4081bed04d5d2ea8e2807f665a2456354ed
Wesley Hindle <wesleyhindle@gmail.com>
2021-08-17 11:11:23 +0100

    Test to check for fields for login info and submit button's presence passing.

--- spec/features/login_page_spec.rb
+++ spec/features/login_page_spec.rb
@@ -2,7 +2,13 @@ feature 'Login page 'do
   scenario 'Viewing login page ' do
     visit '/login'
     expect(page).to have_content "Login page"
-  
-  
   end   
+
+  scenario 'User can add details to log in ' do 
+    visit '/login'
+    fill_in('username', with: "testlogin1")
+    fill_in('password', with: "testpassword1")
+    click_button('login')
+  end
+
 end 
