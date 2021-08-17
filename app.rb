70ddb01df66ab3c4eca2d76706ee01b8bfa22be5
Wesley Hindle <wesleyhindle@gmail.com>
2021-08-17 12:29:22 +0100

    Re factored and started to add unit tests for the User class

--- app.rb
+++ app.rb
@@ -3,6 +3,10 @@ require_relative 'lib/user.rb'
 
 class Makers_BnB < Sinatra::Base 
 
+  get '/' do 
+    "Sign up page"
+   end
+
   get '/login' do 
     erb :login
   end 
@@ -10,17 +14,17 @@ class Makers_BnB < Sinatra::Base
   post '/testinglogin' do 
     p @login = User.auth(username: params[:username], password: params[:password])
 
-    if @login == true
-      redirect '/'
+    if @login == true #Can we remove the logic?
+      redirect '/spaces'
     else
       redirect '/login'
     end
+    
   end 
 
-  get '/' do 
-   "Welcome back"
+  get '/spaces' do
+    erb :spaces
   end
-
 
   run! if app_file == $0
 end
