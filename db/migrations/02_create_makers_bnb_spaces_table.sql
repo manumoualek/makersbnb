13872e09bb5438b713543b824818cbdff082ab79
NazirShaikh <shain003.212@gmail.com>
2021-08-18 15:50:43 +0100

    changed data type of space_price to DECIMAL instead of VARCHAR

--- db/migrations/02_create_makers_bnb_spaces_table.sql
+++ db/migrations/02_create_makers_bnb_spaces_table.sql
@@ -4,7 +4,7 @@ CREATE TABLE spaces (
 spaceID SERIAL PRIMARY KEY,
 space_name VARCHAR (60),
 space_description VARCHAR (60),
-space_price VARCHAR (60),
+space_price DECIMAL (10, 2),
 available_from DATE,
 available_to DATE
 ); 
@@ -15,7 +15,7 @@ CREATE TABLE spaces (
 spaceID SERIAL PRIMARY KEY,
 space_name VARCHAR (60),
 space_description VARCHAR (60),
-space_price VARCHAR (60),
+space_price DECIMAL (10, 2),
 available_from DATE,
 available_to DATE
 ); 
