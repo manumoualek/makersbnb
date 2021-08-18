c3eb444f25731897059f20a9b9449e16d1847547
manu <manuelmoualek@gmail.com>
2021-08-18 13:13:52 +0100

    Updated auth method tests and removed initialize method tests

--- spec/unit/user_spec.rb
+++ spec/unit/user_spec.rb
@@ -5,32 +5,13 @@ describe User do
   describe '#auth' do 
     it 'redirects to the spaces page if username and passwords match' do 
       temp = User.auth(username: 'testlogin1', password:'testpassword1')
-      expect(temp).to eq "/spaces"
-      #expect(temp.logged_in).to_be true
+      expect(temp).to eq true
     end 
 
 
     it 'redirects to the login page if the username and passwords DO NOT match' do 
       temp = User.auth(username: 'testlogin1', password:'WRONGPASSWORD')
-      expect(temp).to eq "/login"
+      expect(temp).to eq false
     end 
   end 
-
-  describe '#initialize' do
-    it 'returns nil when initialized' do 
-      temp = User.new
-      expect(temp.logged_in).to eq nil
-    end 
-
-    it 'returns true when the log in info is correct' do 
-      
-      
-    end 
-
-    it 'returns false when log in info is incorrect' do
-    end
-  end
-
-
-
 end 
