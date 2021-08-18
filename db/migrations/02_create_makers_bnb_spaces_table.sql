33a4d6daeee5fe3c01dbac7c9baf899a9f9ea8b3
Dylan <Dylan@eequ.org>
2021-08-18 10:47:00 +0100

    added some readability

--- db/migrations/02_create_makers_bnb_spaces_table.sql
+++ db/migrations/02_create_makers_bnb_spaces_table.sql
@@ -1,3 +1,14 @@
+CREATE DATABASE MakersBnB; 
+
+CREATE TABLE spaces (
+spaceID SERIAL PRIMARY KEY,
+space_name VARCHAR (60),
+space_description VARCHAR (60),
+space_price VARCHAR (60)
+); 
+
+CREATE DATABASE MakersBnB_test; 
+
 CREATE TABLE spaces (
 spaceID SERIAL PRIMARY KEY,
 space_name VARCHAR (60),
