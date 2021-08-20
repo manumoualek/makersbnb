82525c52f478e5d53304b6a02f3e5816a6e9fbea
Wesley Hindle <wesleyhindle@gmail.com>
2021-08-20 12:59:40 +0100

    Started to write a test for displaying the requested spaces, but need to merge before can continue

--- lib/request.rb
+++ lib/request.rb
@@ -2,14 +2,11 @@ require 'database_connection.rb'
 class Request
   
   def self.create(space_id: , check_out: , check_in: , guest: , host: , approved:)
-
     db_connect.exec(
       "INSERT INTO requests (space_id, check_out , check_in , guest , host , approved) 
        VALUES ('#{space_id}', '#{check_out}', 
       '#{check_in}', '#{guest}', '#{host}', '#{approved}');"
     )
-
-
   end  
 
 end  
