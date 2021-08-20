8697ed916ed883e66f7ca646953e50fa85d79bb6
manu <manuelmoualek@gmail.com>
2021-08-20 10:48:56 +0100

    added spaces into setupdatabase file to automatically create a space and updated corresponding unit test

--- spec/setup_test_database.rb
+++ spec/setup_test_database.rb
@@ -4,4 +4,5 @@ def setup_test_database
   connection = PG.connect(dbname: 'makersbnb_test')
   connection.exec("TRUNCATE users, spaces, requests RESTART IDENTITY;") #Everytime the test DB is run here it removes all previous primary keys (for all specified tables) and restarts the primary key counter at 1. This makes testing possible.
   connection.exec("INSERT INTO users (username, password, first_name, second_name, email) values ('testlogin1', 'testpassword1', 'testforename1', 'testsurname1', 'test1@email.com');")
+  connection.exec("INSERT INTO spaces (space_name, userid, space_description, space_price, available_from, available_to) values ('testspace1', '1', 'This is the test space', '99.99', '2022-05-05', '2022-05-06');")
 end
--- spec/unit/space_spec.rb
+++ spec/unit/space_spec.rb
@@ -3,16 +3,14 @@ describe "space" do
     it "Should add a space to the database" do
       connection = PG.connect(dbname: 'makersbnb_test')
 
-        Space.create(space_name: "Valley of the end", space_description: "Very big valley with lots of end yes", 
-          space_price: "95.00", available_from: "2021/05/05", available_to: "2021/05/06", userID: '1')
-
         result = connection.exec("SELECT * FROM spaces;").to_a
 
-        expect(result[0]['space_name']).to eq('Valley of the end')
-        expect(result[0]['space_description']).to eq('Very big valley with lots of end yes')
-        expect(result[0]['space_price']).to eq('95.00')
-        expect(result[0]['available_from']).to eq('2021-05-05')
-        expect(result[0]['available_to']).to eq('2021-05-06')
+        expect(result[0]['space_name']).to eq('testspace1')
+        expect(result[0]['space_description']).to eq('This is the test space')
+        expect(result[0]['space_price']).to eq('99.99')
+        expect(result[0]['available_from']).to eq('2022-05-05')
+        expect(result[0]['available_to']).to eq('2022-05-06')
+        expect(result[0]['userid']).to eq ('1')
     end
   end
 end
